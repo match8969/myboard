68b83ae097071e08471970b3604f75092930ea70
町田龍浩 <match8969@gmail.com>
2019-06-07 19:59:34 +0900

    Add show update in test mode. #14

--- app/controllers/admin/instagram_accounts_controller.rb
+++ app/controllers/admin/instagram_accounts_controller.rb
@@ -12,6 +12,45 @@ class Admin::InstagramAccountsController < ApplicationController
   # GET /instagram_accounts/1
   # GET /instagram_accounts/1.json
   def show
+
+    # TODO: accountのavatarのアップデート
+    #
+
+    # TODO: contentsのアップデート(あとでserviceに任せよう,,,)
+    # Notification: cacheなので即時反映のテストはできない、、、
+    # 1. accountのlatestなcontent(idがlast)のimageを取得する
+    # 2, スクレイピングでimage_pathsを取得
+    # 3. 条件分岐
+
+    latest_image_path = @instagram_account.instagram_contents.last.image
+    puts "----- test 1 -----"
+
+    # newからのリダイレクトでは更新は行わない
+    return @instagram_account if @instagram_account.updated_at > Time.zone.now - 10.second # OK! new の時はwithin 10 secやった
+
+    puts "test 2"
+
+    new_image_paths = InstagramContentImageService.new(@instagram_account.account_name).fetch_image_paths
+    current_image_paths = @instagram_account.instagram_contents.pluck(:image)
+    diff_image_paths = new_image_paths - current_image_paths
+
+    puts "current_image_paths= #{current_image_paths}"
+    puts "new_image_paths = #{new_image_paths}"
+    puts "diff_image_paths=#{diff_image_paths}"
+    puts "test 3"
+
+
+    return @instagram_account if diff_image_paths.blank?
+
+    puts "test 4"
+
+    diff_image_paths.each do |image_path|
+      instagram_content = @instagram_account.instagram_contents.new(image: image_path)
+      instagram_content.save
+    end
+    puts "----- test 5 -----"
+    @instagram_account
+
   end
 
   # GET /instagram_accounts/new
